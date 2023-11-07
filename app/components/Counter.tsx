ffcec3d21f2e5428414d67491c9b5ef9c0de8ecf
vineetsingh0809 <ankitchilma@gmail.com>
2023-11-07 16:52:13 +0530

    Applied the condition to show install banner conditionally

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const Counter = () => {
@@ -24,6 +25,7 @@ const Counter = () => {
 
       deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
         if (choiceResult.outcome === "accepted") {
+          container.remove();
           console.log("User accepted the install prompt");
         } else {
           console.log("User dismissed the install prompt");
@@ -37,6 +39,12 @@ const Counter = () => {
   let deferredPrompt: any;
 
   useEffect(() => {
+    if (window.matchMedia("(display-mode: standalone)").matches) {
+      setShowBtn(false);
+    } else {
+      setShowBtn(true);
+    }
+
     window.addEventListener("beforeinstallprompt", (e) => {
       if (/Android/i.test(navigator.userAgent)) {
         setShowBtn(true);
@@ -52,18 +60,27 @@ const Counter = () => {
       {showBtn && (
         <div
           id="container"
-          className="w-1/3 max-[480px]:w-full m-auto flex items-center justify-around bg-gray-200 rounded-md py-4 absolute bottom-0 left-[50%] translate-x-[-50%]"
+          className="w-1/3 max-[480px]:w-full m-auto flex items-center justify-between bg-gray-200 rounded-md py-4 px-2 absolute bottom-0 left-[50%] translate-x-[-50%]"
         >
-          <h1 className="text-black text-xl max-[480px]:text-base">Install the app in your device</h1>
-          {
-            <button
-              id="installBTN"
-              className="bg-blue-300 text-black px-6 py-2 rounded-md border-none"
-              onClick={handleInstallClick}
-            >
-              Install
-            </button>
-          }
+          <div className="flex items-center">
+            <Image
+              src={"/icon-384x384.png"}
+              alt="app icon"
+              width={60}
+              height={60}
+            />
+            <h1 className="text-black text-lg max-[480px]:text-base ml-3">
+              NEXT PWA
+            </h1>
+          </div>
+
+          <button
+            id="installBTN"
+            className="bg-blue-300 text-black px-6 py-2 rounded-2xl border-none"
+            onClick={handleInstallClick}
+          >
+            Install
+          </button>
         </div>
       )}
       <div className="w-full h-[100vh] flex items-center justify-center">
